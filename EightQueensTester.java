6d69fb938ec6be4d736d70d15ad9215559291c69
khaled-hamada <khaledosman737@gmail.com>
2018-12-23 12:42:45 +0200

    Update EightQueensTester.java

--- EightQueensTester.java
+++ EightQueensTester.java
@@ -10,41 +10,54 @@ package eightqueens;
  * @author Kahled Osman
  */
 public class EightQueensTester {
-    
+   // public static int sol[][] ;
     public static void main(String[] args) {
+        //indicating whether to run the safe algorithm of the blind one 
+        boolean runSafeAlgorithm = false;
         long startTime = System.currentTimeMillis();
         
-        DFS d = new DFS(60);
+        DFS d = new DFS(10 , runSafeAlgorithm);
         State s = d.getSolution();
         int gStates = d.getGeneratedStatesNumber();
         int exStates = d.getExploredStatesNumber();
         int nQueens = d.getQueensNumber();
         printSolution(s, nQueens);
+       // sol =  new int[nQueens][nQueens];
         
-      
         System.out.println("Number of Queens is \n "+nQueens);
         //System.out.println("number of solutions \n" + solutionCount);
         System.out.println("number of generated states  = \n"+ gStates);
         System.out.println("number of explored  states  = \n"+ exStates);
         long FinishTime = System.currentTimeMillis();
         System.out.println("finishing time is : \n"+ (FinishTime - startTime)+ " MS");
-
+       // System.out.println("Solution \n"+sol.toString());
+       
+       
+       
 	}
      //i will edit this method to return  1-D array contains the queens columns numbers only 
     // like this [ 2,4,7,6,8,......] 
         public static void  printSolution(State state , int nQueens){
-            for(Point goalpoint : state.getPointList()){
-                for(int i=0; i<nQueens; i++){
-                        if(i != goalpoint.getY())
-                                System.out.print("_ ");
-                        else
-                                System.out.print("Q ");
-                }
-                System.out.println(); 
+            if(state != null)
+                for(Point goalpoint : state.getPointList()){
+                    for(int i=0; i<nQueens; i++){
+                            if(i != goalpoint.getY()){
+                                    System.out.print("_ ");
+                                  //  sol[i][goalpoint.getY()] = 0;
+                            }
+                            else{
+                                    System.out.print("Q ");
+                                 //   sol[i][goalpoint.getY()] = 1;
+                            }
+                    }
+                    System.out.println(); 
                
         }
+        else
+                System.out.println("NO solution has been found ");
         System.out.println();
             
         }
-
+        
+       
 }
